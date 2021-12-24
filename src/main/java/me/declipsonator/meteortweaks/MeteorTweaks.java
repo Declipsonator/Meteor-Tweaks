50e575221820fcbeb5e0708e57a9afe7506eebf6
Declipsonator <96392121+Declipsonator@users.noreply.github.com>
2021-12-23 21:55:25 -0500

    Add files via upload

---
+++ src/main/java/me/declipsonator/meteortweaks/MeteorTweaks.java
@@ -0,0 +1,34 @@
+package me.declipsonator.meteortweaks;
+
+import me.declipsonator.meteortweaks.modules.Scaffold;
+import me.declipsonator.meteortweaks.modules.hud.HudExample;
+import meteordevelopment.meteorclient.MeteorClient;
+import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.systems.modules.render.hud.HUD;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.lang.invoke.MethodHandles;
+
+public class MeteorTweaks extends MeteorAddon {
+	public static final Logger LOG = LogManager.getLogger();
+
+	@Override
+	public void onInitialize() {
+		LOG.info("Initializing Meteor Addon Template");
+
+		// Required when using @EventHandler
+		MeteorClient.EVENT_BUS.registerLambdaFactory("me.declipsonator.meteortweaks", (lookupInMethod, klass) -> (MethodHandles.Lookup) lookupInMethod.invoke(null, klass, MethodHandles.lookup()));
+
+		// Modules
+		Modules.get().add(new Scaffold());
+
+		// HUD
+		HUD hud = Modules.get().get(HUD.class);
+		hud.elements.add(new HudExample(hud));
+
+
+	}
+
+}
